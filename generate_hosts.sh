683b2970bdb57694d0b8e6611bd660e2eefcb29c
YK Liu <cos.lyk@gmail.com>
2021-12-31 15:09:54 +0800

    Fix format

--- generate_hosts.sh
+++ generate_hosts.sh
@@ -1,10 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 
 cat websites.txt | while read LINE; do
+    echo "$LINE"
     if [ "${LINE:0:1}" = "#" -o -z "${LINE}" ]; then
         echo "${LINE}" >> hosts
     else
-        host -4 "${LINE}" | grep -P '\d+\.\d+\.\d+\.\d+' -o -m 1 >> hosts
+        IP=`host -4 "${LINE}" | grep -P '\d+\.\d+\.\d+\.\d+' -o -m 1`
+        echo "$IP $LINE" >> hosts
         sleep 0.2
     fi
 done
