223d8b23232234ac706dba315390a1881f158524
刘奕坤 <liuyikun@192.168.10.3>
2022-02-19 21:19:56 +0800

    add websites

--- generate_hosts.sh
+++ generate_hosts.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+cat > hosts <<\EOF
+##
+# Host Database
+#
+# localhost is used to configure the loopback interface
+# when the system is booting.  Do not change this entry.
+##
+127.0.0.1	localhost
+255.255.255.255	broadcasthost
+::1             localhost
+EOF
+
 cat websites.txt | while read LINE; do
     echo "$LINE"
     if [ "${LINE:0:1}" = "#" -o -z "${LINE}" ]; then
